add531811a2abd78bb93bc037cc8641867c937d6
ems541a <ems541a@gmail.com>
2017-03-23 13:42:16 -0700

    Removing fake console

---
+++ main_5.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+import inspect
+import io
+import sys
+import cmd
+import shutil
+
+"""
+ Cleanup file storage
+"""
+import os
+file_path = "file.json"
+if not os.path.exists(file_path):
+    try:
+        from models.engine.file_storage import FileStorage
+        file_path = FileStorage._FileStorage__file_path
+    except:
+        pass
+if os.path.exists(file_path):
+    os.remove(file_path)
+
+"""
+ Backup console file
+"""
+if os.path.exists("tmp_console_main.py"):
+    shutil.copy("tmp_console_main.py", "console.py")
+shutil.copy("console.py", "tmp_console_main.py")
+
+"""
+ Backup models/__init__.py file
+"""
+if os.path.exists("models/tmp__init__.py"):
+    shutil.copy("models/tmp__init__.py", "models/__init__.py")
+shutil.copy("models/__init__.py", "models/tmp__init__.py")
+
+"""
+ Overwrite models/__init__.py file with switch_to_file_storage.py
+"""
+if os.path.exists("switch_to_file_storage.py"):
+    shutil.copy("switch_to_file_storage.py", "models/__init__.py")
+
+
+"""
+ Updating console to remove "__main__"
+"""
+with open("tmp_console_main.py", "r") as file_i:
+    console_lines = file_i.readlines()
+    with open("console.py", "w") as file_o:
+        in_main = False
+        for line in console_lines:
+            if "__main__" in line:
+                in_main = True
+            elif in_main:
+                if "cmdloop" not in line:
+                    file_o.write(line.lstrip("    ")) 
+            else:
+                file_o.write(line)
+
+import console
+
+"""
+ Create console
+"""
+console_obj = "HBNBCommand"
+for name, obj in inspect.getmembers(console):
+    if inspect.isclass(obj) and issubclass(obj, cmd.Cmd):
+        console_obj = obj
+
+my_console = console_obj(stdout=io.StringIO(), stdin=io.StringIO())
+my_console.use_rawinput = False
+
+"""
+ Exec command
+"""
+def exec_command(my_console, the_command, last_lines = 1):
+    my_console.stdout = io.StringIO()
+    real_stdout = sys.stdout
+    sys.stdout = my_console.stdout
+    my_console.onecmd(the_command)
+    sys.stdout = real_stdout
+    lines = my_console.stdout.getvalue().split("\n")
+    return "\n".join(lines[(-1*(last_lines+1)):-1])
+
+"""
+ Tests
+"""
+state_name = "California"
+result = exec_command(my_console, "create State name=\"{}\"".format(state_name))
+if result is None or result == "":
+    print("FAIL: No ID retrieved")
+state_id = result
+
+city_name = "San Francisco is super cool"
+result = exec_command(my_console, "create City state_id=\"{}\" name=\"{}\"".format(state_id, city_name.replace(" ", "_")))
+if result is None or result == "":
+    print("FAIL: No ID retrieved")
+city_id = result
+
+result = exec_command(my_console, "show City {}".format(city_id))
+if result is None or result == "":
+    print("FAIL: empty output")
+if "[City]" not in result or city_id not in result:
+    print("FAIL: wrong output format: \"{}\"".format(result))
+if "name" not in result or city_name not in result:
+    print("FAIL: missing new information: \"{}\"".format(result))
+if "state_id" not in result or state_id not in result:
+    print("FAIL: missing new information: \"{}\"".format(result))
+print("OK", end="")
+
+shutil.copy("tmp_console_main.py", "console.py")
+shutil.copy("models/tmp__init__.py", "models/__init__.py")
