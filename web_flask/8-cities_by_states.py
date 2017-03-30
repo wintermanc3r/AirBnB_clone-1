8914734d36b8769ae2daf22d90f0201934f62e32
ems541a <ems541a@gmail.com>
2017-03-29 18:41:31 -0700

    Removed unnecessary loop

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -94,10 +94,8 @@ def states_list():
 @app.route("/cities_by_states")
 def cities_by_states():
     try:
-        states = storage.all('State')
-        for state in states.keys():
-            return render_template('8-cities_by_states.html',
-                                   states=storage.all('State'))
+        return render_template('8-cities_by_states.html',
+                               states=storage.all('State'))
     except:
         return render_template('8-cities_by_states.html',
                                states={})
