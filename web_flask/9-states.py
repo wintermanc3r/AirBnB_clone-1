4f4384518373cb377f642e3d2a2913d070b85ed9
ems541a <ems541a@gmail.com>
2017-03-29 22:08:24 -0700

    Initial push of 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""
+Super simple Flask app.
+"""
+from flask import Flask
+from flask import abort, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+@app.route("/")
+def main():
+    """
+    Default route.
+    """
+    return "Hello HBNB"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    hbnb directory
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_dir(text):
+    """
+    Returns C and the text sent in.
+    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_dir(text="is cool"):
+    """
+    Returns python and the text.
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route("/number/<n>")
+def number(n):
+    """
+    Returns n, if it's an integer
+    """
+    try:
+        return ("{} is a number".format(int(n)))
+    except:
+        abort(404)
+
+
+@app.route("/number_template/<n>")
+def number_template(n):
+    """
+    Returns n, if it's an integer
+    """
+    try:
+        n = int(n)
+        return render_template('5-number.html', n=n)
+    except:
+        abort(404)
+
+
+@app.route("/number_odd_or_even/<n>")
+def number_odd_or_even_template(n):
+    """
+    Returns n, if it's an integer
+    """
+    try:
+        n = int(n)
+        if n % 2 == 0:
+            s = "even"
+        else:
+            s = "odd"
+        return render_template('6-number_odd_or_even.html', n=n, s=s)
+    except:
+        abort(404)
+
+
+@app.route("/states/<id>")
+def specific_state(id):
+    try:
+        state = [storage.all('State')[str(id)]]
+        name = "State: {}".format(state[0].name)
+    except KeyError:
+        state = []
+        name = "Not found!"
+    return render_template('9-states.html', states=state, name=name)
+
+
+@app.route("/states")
+def all_state():
+    name = "States"
+    state = [x for x in storage.all('State').values()]
+    return render_template('9-states.html',
+                           states=state, name=name)
+
+
+
+if __name__ == "__main__":
+    app.run()
